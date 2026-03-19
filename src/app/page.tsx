d694e8fa4a913dd31ad92170d7af9bac2a26d843
bender <kudin.dima@gmail.com>
2026-03-19 13:09:39 +0000

    Update src/app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -63,7 +63,7 @@ export default function BarberHousePage() {
               id: "1",              name: "Nam Le-Hoang",              handle: "@namlehoang",              imageSrc: "https://webuild-dev.s3.eu-north-1.amazonaws.com/users/user_3AiJU1e2jhOFKhLWY9tVwfVdNYC/uploaded-1773925606192-ozb9ewdk.png",              imageAlt: "Nam Le-Hoang",              testimonial: "Barbři se vyznají v práci! Naprostá spokojenost"
             },
             {
-              id: "2",              name: "Radovan Jansa",              handle: "@radovanjansa",              imageSrc: "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?w=400&h=400&fit=crop",              imageAlt: "Radovan Jansa",              testimonial: "Skvělé služby a haircut. Teď také výrazně rozšířili a zmodernizovali prostory holičství. Určitě doporučuji 👍💈✂️💇‍♂️"
+              id: "2",              name: "Radovan Jansa",              handle: "@radovanjansa",              imageSrc: "https://webuild-dev.s3.eu-north-1.amazonaws.com/users/user_3AiJU1e2jhOFKhLWY9tVwfVdNYC/uploaded-1773925719532-rkm6swdp.png",              imageAlt: "Radovan Jansa",              testimonial: "Skvělé služby a haircut. Teď také výrazně rozšířili a zmodernizovali prostory holičství. Určitě doporučuji 👍💈✂️💇‍♂️"
             },
             {
               id: "3",              name: "Nam Trương",              handle: "@namtruong",              imageSrc: "https://images.unsplash.com/photo-1531746020798-e6953c6e8e04?w=400&h=400&fit=crop",              imageAlt: "Nam Trương",              testimonial: "Zkušení holiči. Vřele doporučuji."
@@ -72,7 +72,7 @@ export default function BarberHousePage() {
               id: "4",              name: "Danh Phạm",              handle: "@danhpham",              imageSrc: "https://images.unsplash.com/photo-1522228115018-61b2f0398a1e?w=400&h=400&fit=crop",              imageAlt: "Danh Phạm",              testimonial: "Tento Barber shop je fantastické a holiči jsou zde velmi profesionální a pohlední."
             },
             {
-              id: "5",              name: "Achala Fernando",              handle: "@achalafernando",              imageSrc: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?w=400&h=400&fit=crop",              imageAlt: "Achala Fernando",              testimonial: "Velmi dobrá obsluha! Vřele doporučuji..."
+              id: "5",              name: "Achala Fernando",              handle: "@achalafernando",              imageSrc: "https://webuild-dev.s3.eu-north-1.amazonaws.com/users/user_3AiJU1e2jhOFKhLWY9tVwfVdNYC/uploaded-1773925669028-4jxwa0y7.png",              imageAlt: "Achala Fernando",              testimonial: "Velmi dobrá obsluha! Vřele doporučuji..."
             }
           ]}
           animationType="slide-up"
